b810a0d50b42d0efd5bc1b9d17c6bdede3696780
Ryan Devenney <rwdevenney@gmail.com>
2022-08-20 17:48:42 -0400

    clean prints

--- src/commands/projects.rs
+++ src/commands/projects.rs
@@ -9,11 +9,9 @@ pub fn add_project(db: &Connection) {
 
     read_input("Project title:".to_string(), &mut title).expect("Failed to read project name");
 
-    println!("Project description:");
     read_input("Project description: ".to_string(), &mut description)
         .expect("Failed to read project description");
 
-    println!("Assign to team member:");
     read_input("Assign to team member:".to_string(), &mut assignee)
         .expect("Failed to read project description");
 }
